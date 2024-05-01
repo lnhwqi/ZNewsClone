e05a2b195e2056bdc812f00b40284a4859134317
huy <lnhuy2024@gmail.com>
2024-05-01 13:56:55 +0700

    FIx Responsive #3

--- script.js
+++ script.js
@@ -2,16 +2,20 @@ const searchBtn = document.getElementById('search-btn')
 const searchBar = document.getElementById('search-bar')
 const moreSign = document.getElementById('more-sign')
 const closeSign = document.getElementById('close-sign')
+
 const moreListHead = document.getElementById('more-list-head')
 const moreListBottom = document.getElementById('more-list-bottom')
 
 const mainCenterDesc = document.getElementById('main-center-desc')
-
+const blur = document.getElementById('blur-box')
 
 if (window.matchMedia("(max-width: 768px)").matches) {
     if(mainCenterDesc.textContent.split(' ').length > 30){
         mainCenterDesc.textContent = mainCenterDesc.textContent.split(' ').slice(0,23).join(' ') + ' ...'
     }
+
+    moreSign.classList.remove('fa-ellipsis');
+    moreSign.classList.add('fa-bars');
 }
 
 document.body.addEventListener('click', function(event){
@@ -28,8 +32,12 @@ document.body.addEventListener('click', function(event){
         moreListHead.classList.remove('visibility');
         closeSign.classList.toggle('visibility')
         moreSign.classList.toggle('hidden')
+        blur.style.display = 'none';
+        document.documentElement.style.overflow = 'auto';    
     }
 })
+
+
 searchBtn.addEventListener('click', function(){
     searchBtn.classList.toggle('active')
     searchBar.classList.toggle('visibility');
@@ -47,11 +55,17 @@ moreSign.addEventListener('click', function(){
     moreSign.classList.toggle('hidden')
     moreListHead.classList.toggle('visibility')
     moreListBottom.classList.toggle('visibility')
+    blur.style.display = 'block';
+    document.documentElement.style.overflow = 'hidden';
 })
 closeSign.addEventListener('click', function(){
     closeSign.classList.toggle('visibility')
     moreSign.classList.toggle('hidden')
     moreListHead.classList.toggle('visibility')
     moreListBottom.classList.toggle('visibility')
+    blur.style.display = 'none';
+    document.documentElement.style.overflow = 'auto';
 })
+
+
 
